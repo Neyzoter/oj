b956f3fff067f5b763461347a88d5b75b5e47b1d
Neyzoter <sonechaochao@gmail.com>
2019-12-08 22:09:35 +0800

    update

--- src/main/java/cn/neyzoter/oj/hash/SingleNumber.java
+++ src/main/java/cn/neyzoter/oj/hash/SingleNumber.java
@@ -1,13 +1,18 @@
 package cn.neyzoter.oj.hash;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 136.只出现一次的数字
  */
 public class SingleNumber {
     public static void main(String[] args) {
         int[] nums = {2,2,1};
-        Solution1_SingleNumber solution1_singleNumber = new Solution1_SingleNumber();
-        System.out.println(solution1_singleNumber.singleNumber(nums));
+//        Solution1_SingleNumber solution1_singleNumber = new Solution1_SingleNumber();
+//        System.out.println(solution1_singleNumber.singleNumber(nums));
+        Solution2_SingleNumber solution2_singleNumber = new Solution2_SingleNumber();
+        System.out.println(solution2_singleNumber.singleNumber(nums));
     }
 }
 
@@ -31,6 +36,30 @@ class Solution1_SingleNumber {
                 found = false;
             }
         }
+        return 0;
+    }
+}
+
+/**
+ * Hash法
+ */
+class Solution2_SingleNumber {
+    public int singleNumber(int[] nums) {
+        Map<Integer, Integer> hashmap = new HashMap<>();
+        for (int num : nums) {
+            if (hashmap.containsKey(num)) {
+                hashmap.put(num, 2) ;
+            } else {
+                hashmap.put(num, 1);
+            }
+        }
+
+        for (int num : nums) {
+            if (hashmap.get(num) == 1) {
+                return num;
+            }
+        }
+
         return 0;
     }
 }
