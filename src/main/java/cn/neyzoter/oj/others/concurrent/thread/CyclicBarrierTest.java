42dd0430a01a757d410cb5380ae2ccd88fb891df
Neyzoter <sonechaochao@gmail.com>
2020-02-18 16:47:54 +0800

    CyclicBarrierTest

---
+++ src/main/java/cn/neyzoter/oj/others/concurrent/thread/CyclicBarrierTest.java
@@ -0,0 +1,56 @@
+package cn.neyzoter.oj.others.concurrent.thread;
+
+import java.util.Timer;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * test cyclic barries test
+ * @author Neyzoter Song
+ * @date 2020-2-18
+ */
+public class CyclicBarrierTest {
+    public static int ThreadNum = 5;
+    public static CyclicBarrier cyclicBarrier = new CyclicBarrier(ThreadNum,new CyclicBarrierRunnable());
+    public static void main (String[] args) {
+        for (int i = 0; i < ThreadNum ;i ++ ){
+            try{
+                Thread.sleep(50);
+                new CyclicBarrierTask(cyclicBarrier,i).start();
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+
+        }
+    }
+
+
+}
+
+class CyclicBarrierTask extends Thread {
+    public CyclicBarrier cyclicBarrier;
+    public int threadId;
+    public CyclicBarrierTask(CyclicBarrier cyclicBarrier, int id) {
+        this.cyclicBarrier = cyclicBarrier;
+        this.threadId = id;
+    }
+    @Override
+    public void run() {
+        try{
+            Thread.sleep(100);
+            System.out.println(String.format("[%d]Task[%d] Over, Wait for all tasks over!", System.currentTimeMillis(),this.threadId));
+            this.cyclicBarrier.await(10, TimeUnit.SECONDS);
+        } catch (Exception e) {
+            System.err.println(e);
+        } finally {
+            System.out.println(String.format("Task[%d] exited!", this.threadId));
+        }
+    }
+}
+
+class CyclicBarrierRunnable implements Runnable {
+    @Override
+    public void run() {
+        System.out.println("Tasks Over!");
+    }
+}
