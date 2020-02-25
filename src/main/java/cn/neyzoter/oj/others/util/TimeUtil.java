4d64a41dedc3ba642e5acbaad5b6b63a0da23649
Neyzoter <sonechaochao@gmail.com>
2020-02-25 20:17:46 +0800

    add time util

---
+++ src/main/java/cn/neyzoter/oj/others/util/TimeUtil.java
@@ -0,0 +1,16 @@
+package cn.neyzoter.oj.others.util;
+
+/***
+ * time util
+ * @author Neyzoter Song
+ * @date 2020-2-25
+ */
+public class TimeUtil {
+    public static void getMs() {
+        System.out.println(System.currentTimeMillis());
+    }
+
+    public static void main (String[] args) {
+        TimeUtil.getMs();
+    }
+}
