ae7ea2dfce0efe8415edb89c40c0a252c0580b3b
Neyzoter <sonechaochao@gmail.com>
2020-02-25 11:43:56 +0800

    add TestStaticStringAsFormat

---
+++ src/main/java/cn/neyzoter/oj/test/TestStaticStringAsFormat.java
@@ -0,0 +1,12 @@
+package cn.neyzoter.oj.test;
+
+/**
+ * test static String as String.format()
+ */
+public class TestStaticStringAsFormat {
+    public static final String FORMAT = "%s,%s %s %s";
+    public static void main (String[] args) {
+        String printStr = String.format(FORMAT,"measurement", "tags", "fields", "timestamp");
+        System.out.println(printStr);
+    }
+}
