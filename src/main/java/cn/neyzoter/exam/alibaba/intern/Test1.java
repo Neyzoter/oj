86f2a0b83f2252607899fd39910e86a535468436
Neyzoter <sonechaochao@gmail.com>
2020-04-16 16:01:37 +0800

    finish alibaba intern test1

--- src/main/java/cn/neyzoter/exam/alibaba/intern/Test1.java
+++ src/main/java/cn/neyzoter/exam/alibaba/intern/Test1.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
  * 森林之王<br/>
  * 注意类名必须为Main, 不要有任何package xxx信息
  * @author Charles Song
- * @date 2020-3
+ * @date 2020-4-13
  */
 public class Test1 {
     public static void main (String[] args) {
@@ -21,23 +21,38 @@ public class Test1 {
             count(num, A);
         }
     }
+
+    /**
+     * 计算每个动物可能收到的票数，并打印
+     * @param num 动物数目
+     * @param A 动物崇拜情况
+     */
     public static void count (int num, int[] A) {
         int[] sum = new int[num];
         for (int i = 0; i < num ; i ++) {
-            int idx = A[i] - 1;
-            if (idx >= 0) {
-                if (idx != i) {
-                    int admIdx = A[idx] - 1;
-                    if (admIdx >= 0) {
-                        sum[admIdx] ++;
-                    }
-                    sum[idx] ++;
-                }
-            }
-            sum[i] ++;
+            // 投票
+            vote(A,i,sum );
         }
         for (int i = 0 ; i < num ; i ++) {
             System.out.println(sum[i]);
+        }
+    }
+
+    /**
+     * 递归投票
+     * @param A 崇拜情况
+     * @param idx 当前的动物
+     * @param sum 投票情况
+     */
+    public static void vote (int[] A, int idx, int[] sum) {
+        // 崇拜的对象
+        int admIdx = A[idx] - 1;
+        // 投自己
+        sum[idx] ++;
+        // 没有崇拜的对象
+        if (admIdx >= 0) {
+            // 有崇拜对象则和崇拜对象投一样
+            vote(A, admIdx, sum);
         }
     }
 }
