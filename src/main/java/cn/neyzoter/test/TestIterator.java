8b43dce5a2eecf6ab1d4829d2b0ccae440d07507
Neyzoter <sonechaochao@gmail.com>
2020-05-05 15:26:07 +0800

    rm by iter

--- src/main/java/cn/neyzoter/test/TestIterator.java
+++ src/main/java/cn/neyzoter/test/TestIterator.java
@@ -15,6 +15,21 @@ public class TestIterator {
         Collection<String> col = new ArrayList<>(10);
         col.add("123");col.add("234");col.add("345");col.add("456");
         Iterator<String> iter = col.iterator();
+        System.out.println("* Origin");
+        iter.forEachRemaining(System.out::println);
+
+        // 使用iterator实现删除元素
+        iter = col.iterator();
+        for (;iter.hasNext();) {
+            String str = iter.next();
+            if (str.equals("123")) {
+                // 调用remove前必须调用next
+                // 下次remove也必须再次调用next
+                iter.remove();
+            }
+        }
+        System.out.println("* Removed \"123\"");
+        iter = col.iterator();
         iter.forEachRemaining(System.out::println);
     }
 }
