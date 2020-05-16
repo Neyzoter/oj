52a90bfc7ef0154e651a878b7698b16ff78f6537
Neyzoter <sonechaochao@gmail.com>
2020-05-16 18:22:00 +0800

    update splitor

--- src/main/java/cn/neyzoter/test/TestSpliteratorCollection.java
+++ src/main/java/cn/neyzoter/test/TestSpliteratorCollection.java
@@ -10,29 +10,35 @@ import java.util.Spliterator;
  * @date 2020-5-6
  */
 public class TestSpliteratorCollection {
+    public static int STR_NUM = 10000000;
+    public static int THREAD_NUM = 8;
     public static void main (String[] args) {
         testSpliterator();
     }
     public static void testSpliterator () {
         Collection<String> col = new HashSet<>(10000);
-        for (int i = 0; i < 100; i ++) {
+        for (int i = 0; i < STR_NUM; i ++) {
             col.add(i + "x" + (i + 2));
         }
         Spliterator<String> spliterator = col.spliterator();
 
-
-
-        class MyThread extends Thread {
+        class Task implements Runnable {
+            public int runtime = 0;
             @Override
             public void run () {
-                String threadName = this.getName();
+                String threadName = Thread.currentThread().getName();
                 System.out.println(threadName + " running ...");
                 spliterator.trySplit().forEachRemaining(o -> {
                     String str = o;
                     str.toUpperCase();
+                    runtime++;
                 });
+                System.out.println(threadName + " runtime : " + runtime);
             }
         }
 
+        for (int i = 0; i < THREAD_NUM; i ++) {
+            new Thread(new Task()).start();
+        }
     }
 }
