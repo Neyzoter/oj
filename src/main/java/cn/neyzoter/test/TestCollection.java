ec0d09e597eb8315b46581d4369e2dfc5589eae6
Neyzoter <sonechaochao@gmail.com>
2020-06-10 21:36:47 +0800

    add leetcode 491

--- src/main/java/cn/neyzoter/test/TestCollection.java
+++ src/main/java/cn/neyzoter/test/TestCollection.java
@@ -1,5 +1,7 @@
 package cn.neyzoter.test;
 
+import com.google.common.collect.Lists;
+
 import java.util.*;
 
 /**
@@ -12,6 +14,7 @@ public class TestCollection {
         testCollecionRm();
         testCompare();
         testCollectionNotAllowSame();
+        testObjectChange();
     }
 
     /**
@@ -73,6 +76,36 @@ public class TestCollection {
             System.out.println("var1 == var2");
         } else {
             System.out.println("var1 != var2");
+        }
+    }
+
+    public static void testObjectChange () {
+
+        Integer var = new Integer(10);
+        Integer var2 = new Integer(10);
+        if (var2 == var) {
+            System.out.println("var2 == var");
+        } else {
+            System.out.println("var2 != var");
+        }
+
+        List<Apple> list = new LinkedList<>();
+        Apple apple = new Apple(1);
+        list.add(apple);list.add(new Apple(2));list.add(new Apple(3));
+        List<Apple> list2 = new LinkedList<>(list);
+        apple.weight = 10;
+        System.out.println(String.format("list1[0].weight = %d, list2[0].weight = %d", list.get(0).weight, list2.get(0).weight));
+    }
+
+    static class Apple implements Comparable{
+        public int weight;
+        public Apple (int w) {
+            this.weight = w;
+        }
+
+        @Override
+        public int compareTo(Object o) {
+            return weight - ((Apple)o).weight;
         }
     }
 }
