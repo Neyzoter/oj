5abf302d64b067a5889abb43c459e375a56e0098
neyzoter <sonechaochao@gmail.com>
2020-08-12 09:56:50 +0800

    add pdd interviewer prepare

---
+++ src/main/java/cn/neyzoter/test/TestBinary.java
@@ -0,0 +1,12 @@
+package cn.neyzoter.test;
+
+/**
+ * 测试二进制
+ * @author neyzoter
+ */
+public class TestBinary {
+    public static void main(String[] args) {
+        int val = -5;
+        System.out.println("十进制  -5  ： 二进制  " + Integer.toBinaryString(val));
+    }
+}
