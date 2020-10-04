0aa007cea385fd445b13450c4c6bdbe86b0b1145
neyzoter <sonechaochao@gmail.com>
2020-10-04 16:07:47 +0800

    add sword 56

---
+++ src/main/java/cn/neyzoter/sword/_56_OnlyOneNums.java
@@ -0,0 +1,37 @@
+package cn.neyzoter.sword;
+
+/**
+ * 剑指 Offer 56 - I. 数组中数字出现的次数
+ * @author neyzoter
+ */
+public class _56_OnlyOneNums {
+}
+
+class Sol_56_OnlyOneNums {
+    public int[] singleNumbers(int[] nums) {
+        int orx = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            orx ^= nums[i];
+        }
+        int oneBit = 0;
+        while (true) {
+            int bit = orx & 0x1;
+            if (bit == 1) {
+                break;
+            }
+            oneBit++;
+            orx = orx >> 1;
+        }
+        int orx1 = 0, orx2 = 0;
+        for (int n : nums) {
+            int temp = n;
+            n = n >> oneBit;
+            if ((n & 0x1) == 1) {
+                orx1 ^= temp;
+            } else {
+                orx2 ^= temp;
+            }
+        }
+        return new int[]{orx1, orx2};
+    }
+}
